4b467e388fec31fdf226faeb2d52059623e1ea21
Pranay123jaunjal <pranay123jaunjal@gmail.com>
2024-12-18 20:24:11 +0530

    initial commit

---
+++ Server/Models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  userName:{
+    type:String,
+    require:true,
+    trim:true
+
+  },
+  email: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  Bio: {
+    type: String,
+    require: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
